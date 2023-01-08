ede9e49e4e2391b95be72c66a3c57f1c372a4179
Rusty Eddy <rusty@rustyeddy.com>
2023-01-08 10:46:46 -0800

    gave apis a redeye prefix

--- bin/client.sh
+++ bin/client.sh
@@ -2,30 +2,30 @@
 camera=Mac.local
 
 echo -n "Getting health .. "
-curl http://localhost:8000/health
+curl http://localhost:8000/redeye/health
 sleep 1
 echo -n "Getting config .. "
-curl http://localhost:8000/config
+curl http://localhost:8000/redeye/config
 sleep 1
 echo -n "Getting messanger .. "
-curl http://localhost:8000/messanger
+curl http://localhost:8000/redeye/messanger
 sleep 1
 
 echo -n "Get video player status"
-curl http://localhost:8000/video
+curl http://localhost:8000/redeye/video
 sleep 1
 
 echo "Turning video on for 10 seconds .. "
 mosquitto_pub -t camera/${camera} -m on
 sleep 1
-curl http://localhost:8000/video
+curl http://localhost:8000/redeye/video
 sleep 9
 
 echo "Turning video off"
 mosquitto_pub -t camera/${camera} -m off
 sleep 1
 
-curl http://localhost:8000/video
+curl http://localhost:8000/redeye/video
 sleep 1
 
 echo "All done."
