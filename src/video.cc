fab2aa985e030fc771f1d9dc24457bbadc7324d0
Rusty Eddy <rusty@rustyeddy.com>
2023-03-23 21:21:42 -0700

    Working with nvidia

--- src/video.cc
+++ src/video.cc
@@ -16,8 +16,8 @@ std::string get_tegra_pipeline1(int width, int height, int fps) {
     return "nvarguscamerasrc sensor_id=0 ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
 }
 
-std::string gstreamer_pipeline (int capture_width, int capture_height, int display_width, int display_height, int framerate, int flip_method) {
-    return "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(capture_width) + ", height=(int)" +
+std::string gstreamer_pipeline (int sensor, int capture_width, int capture_height, int display_width, int display_height, int framerate, int flip_method) {
+    return "nvarguscamerasrc sensor_id=" + std::to_string(sensor) + " ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(capture_width) + ", height=(int)" +
            std::to_string(capture_height) + ", format=(string)NV12, framerate=(fraction)" + std::to_string(framerate) +
            "/1 ! nvvidconv flip-method=" + std::to_string(flip_method) + " ! video/x-raw, width=(int)" + std::to_string(display_width) + ", height=(int)" +
            std::to_string(display_height) + ", format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
@@ -30,7 +30,8 @@ Video::Video( string camstr )
 
     if ( camstr == "tegra0" || camstr == "tegra1" ) {
         
-	std::string pipeline = gstreamer_pipeline(dims.capture_width,
+	std::string pipeline = gstreamer_pipeline(1,
+                                                  dims.capture_width,
 						  dims.capture_height,
 						  dims.display_width,
 						  dims.display_height,
