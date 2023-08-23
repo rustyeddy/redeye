b44a20018336a5d501f8b27a5703c4b8ed7dcba6
Rusty Eddy <rusty@rustyeddy.com>
2023-08-22 20:36:01 -0700

    LFS png

--- src/filters/filter_bigger.cc
+++ src/filters/filter_bigger.cc
@@ -20,34 +20,3 @@ cv::Mat* FltBigger::filter(cv::Mat* img)
     return img;
 }
 
-void bigger_mouse_callback( int event, int x, int y, int flags, void *param )
-{
-    FltBigger *flt = (FltBigger *) param;
-    cout << "Mouse Event: " << event << ", x: " << x << ", y: " << y;
-
-    switch ( event ) {
-    case EVENT_MOUSEMOVE: 
-        cout << " --- Move " << endl;
-        break;
-
-    case EVENT_LBUTTONDOWN:
-        cout << " --- left button down - start box. " << endl;
-        break;
-
-    case EVENT_LBUTTONUP:
-        cout << " --- left button up. " << endl;
-        break;
-
-    case EVENT_RBUTTONDOWN:
-        cout << " --- right button down. " << endl;
-        break;
-
-    case EVENT_RBUTTONUP:
-        cout << " --- right button up. " << endl;
-        break;
-
-    default:
-        cout << " ---  unknown " << event << endl;
-    }
-
-}
